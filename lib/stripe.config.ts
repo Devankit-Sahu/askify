25b8d7e2f83c0266fdc2972a61e9f71dfab99fc1
Devankit Sahu <dev738288@gmail.com>
2024-10-04 22:41:00 +0530

    change stripe api version

--- lib/stripe.config.ts
+++ lib/stripe.config.ts
@@ -4,7 +4,7 @@ import prisma from "./db.config";
 import { Stripe } from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2024-06-20",
+  apiVersion: "2024-09-30.acacia",
   typescript: true,
 });
 
