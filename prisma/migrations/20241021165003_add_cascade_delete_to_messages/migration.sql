9bd4ad3b51d7992c089b8e0fd198e216b3553dbe
Devankit Sahu <dev738288@gmail.com>
2025-04-02 13:05:02 +0530

    remove prisma/migrations from repo

--- prisma/migrations/20241021165003_add_cascade_delete_to_messages/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "messages" DROP CONSTRAINT "messages_fileId_fkey";
-
--- AddForeignKey
-ALTER TABLE "messages" ADD CONSTRAINT "messages_fileId_fkey" FOREIGN KEY ("fileId") REFERENCES "files"("id") ON DELETE CASCADE ON UPDATE CASCADE;
